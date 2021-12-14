2622648d872c2a15ff85134c30532134de0fc4ad
Luke Shay <shay.luke17@gmail.com>
2021-12-13 18:05:50 -0600

    removed dist

--- packages/utils/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-declare const addJSTSGlob: (dir: string) => string
-declare const isValidHttpMethod: (method: string) => boolean
-declare const cleansePath: (p: string, toRemove: string) => string
-declare const capitalize: (s: string) => string
-declare const require: NodeRequire
-declare const getPkg: (file: string) => any
-export { getPkg, require, addJSTSGlob, isValidHttpMethod, cleansePath, capitalize }
